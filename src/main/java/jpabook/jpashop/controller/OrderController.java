bf7229457273f811e55c93300533df7bdf3a1f50
Jinseonghyun <jinsunghyun9@gmail.com>
2024-06-23 14:37:01 +0900

    주문 목록 검색, 취소

--- src/main/java/jpabook/jpashop/controller/OrderController.java
+++ src/main/java/jpabook/jpashop/controller/OrderController.java
@@ -1,22 +1,26 @@
 package jpabook.jpashop.controller;
 
 import jpabook.jpashop.domain.Member;
+import jpabook.jpashop.domain.Order;
 import jpabook.jpashop.domain.item.Item;
+import jpabook.jpashop.repository.OrderSearch;
 import jpabook.jpashop.service.ItemService;
 import jpabook.jpashop.service.MemberService;
 import jpabook.jpashop.service.OrderService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 @Controller
 @RequiredArgsConstructor
 public class OrderController {
+
+    /**
+     *  상품 주문
+     */
 
     //고객이랑 아이템이랑 다 선택할 수 있어야한다.
     private final OrderService orderService;
@@ -45,5 +49,23 @@ public class OrderController {
         // 핵심비즈니스 로직은 안에서 돌아간다.
         orderService.order(memberId, itemId, count); // 어떤 고객이, 어떤 아이템을, 몇개를 주문할거야
         return "redirect:/orders"; // 주문 내용 목록
+    }
+
+    /**
+     * 주문 내역
+     */
+    @GetMapping("/orders")
+    public String orderList(@ModelAttribute("orderSearch") OrderSearch orderSearch, Model model) {
+        List<Order> orders = orderService.findOrders(orderSearch);
+        model.addAttribute("orders", orders);
+//        model.addAttribute("orderSearch", orderSearch); 이 코드를 위에서 자동으로 해주는 것
+
+        return "order/orderList";
+    }
+
+    @PostMapping("/orders/{orderId}/cancel")
+    public String cancelOrder(@PathVariable("orderId") Long orderId) {
+        orderService.cancelOrder(orderId);
+        return "redirect:/orders";
     }
 }
